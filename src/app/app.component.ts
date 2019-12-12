e11a55ff35a5209b7d89c6f51ee3d83123d6cec8
Juan Ruiz <jaruizg_1@uqvirtual.edu.co>
2019-12-12 16:41:30 -0500

    version 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  nombre = 'Juan';
+  nombre2 = 'juAN anGEl ruIz gil';
+  arreglo: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI = Math.PI;
+  porcentaje = 0.234;
+  salario = 1234.5;
+
+  heroe = {
+    nombre: 'Logan',
+    nombreClave: 'Wolverine',
+    edad: 500,
+
+    direccion: {
+      calle: 'Primera',
+      casa: '20'
+    }
+  };
+
+  valorDePromesa = new Promise (( resolve, reject ) => {
+    setTimeout( () => resolve('Llegó la data'), 3500);
+  });
+
+  fecha = new Date();
+  video = 'uZe_qgMbcsY';
+  activar = false;
+
+}
---
+++ src/app/pipes/capitalizado.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Type } from '@angular/compiler';
+
+@Pipe({name: 'capitalizado'})
+export class CapitalizadoPipe implements PipeTransform {
+    transform(value: string, todas: boolean = true): string {
+
+        value = value.toLowerCase();
+        let nombres = value.split(' ');
+        let retorno = '';
+
+        if (todas) {
+            nombres.forEach(element => {
+                element = element[0].toUpperCase() + element.substring(1);
+                retorno += element + ' ';
+            });
+            return retorno.trim();
+        } else {
+            nombres[0] = nombres[0][0].toUpperCase() + nombres[0].substring(1);
+            return nombres.join(' ');
+        }
+    }
+}
